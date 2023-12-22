fe309b5cc4190521606b9b00b993628005549cb1
sangmin-1007 <dosin0822@gmail.com>
2023-12-22 10:56:13 +0900

    Update GameManager.cs
    1.시간이 촉박할 때 경고하는 기능추가
    2. 싱글톤 선언부 수정

--- cardMatching/Assets/Scripts/GameManager.cs
+++ cardMatching/Assets/Scripts/GameManager.cs
@@ -8,31 +8,55 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    private static GameManager instance;
+    public static GameManager Instance => instance ;
 
+
+    [Header("■ Options")]
+    public float maxTime;
+    public float paneltyTime;
+
+    [Header("■ UI")]
     public Text timeText;
+
+    [Header("■ Object")]
     public GameObject endText;
     public GameObject card;
 
     public GameObject firstCard;
     public GameObject secondCard;
 
+    [Header("■ AudioSource")]
     public AudioSource audioSource;
+    public AudioSource bgmSource;
+
+    [Header("■ AudioClip")]
     public AudioClip match;
     public AudioClip correct;
     public AudioClip incorrect;
 
-    float time;
+   
 
+    float time;
+    float effectTime; // 경고등 깜빡임을 조절하기 위한 시간
     private void Awake()
     {
-        Instance = this;
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
 
     void Start()
     {
         Time.timeScale = 1.0f;
+
+        time = maxTime;
 
         int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
 
@@ -57,12 +81,17 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
+        
+        time -= Time.deltaTime;
         timeText.text = time.ToString("N2");
 
-        if (time > 30.0f)
+        WarningUI();
+
+   
+        if (time <= 0)                      // 시간이 0이 되었을때 게임이 끝나도록 변경
         {
             endText.SetActive(true);
+            bgmSource.Stop();
             Time.timeScale = 0.0f;
         }
     }
@@ -106,8 +135,37 @@ public class GameManager : MonoBehaviour
 
     public void ReGame()
     {
-        // SceneManager.LoadScene("MainScene");
+         //SceneManager.LoadScene("MainScene");
         AdsManager.Instance.ShowRewardAd();
+    }
+
+    private void WarningUI()   // 시간이 촉박할때!
+    {
+        if (time <= maxTime * 0.5)  //남은시간이 절반 이하일때
+        {
+            bgmSource.pitch = 1.1f;
+            timeText.color = Color.yellow;
+        }
+        if (time <= maxTime * 0.2)  // 남은시간이 20% 이하일때
+        {
+            bgmSource.pitch = 1.2f;
+
+            effectTime += Time.deltaTime;
+            if(effectTime < 1f)
+            {
+                timeText.color = new Color(1, 0, 0, 1 - effectTime);
+            }
+            else
+            {
+                timeText.color = new Color(1, 0, 0, effectTime);
+                if(effectTime > 1f)
+                {
+                    effectTime = 0f;
+                }
+            }
+            
+        }
+
     }
 
 }
