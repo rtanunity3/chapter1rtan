c6c2eaaceabf2672b478699ecef82b4805b19c45
leejh0469 <leejh0469@naver.com>
2023-12-22 17:19:32 +0900

    스테이지 선택 창 UI 추가

--- cardMatching/Assets/Scripts/StageSelect.cs
+++ cardMatching/Assets/Scripts/StageSelect.cs
@@ -5,7 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class StageSelect : MonoBehaviour
 {
+    [Header("Object")]
+    public GameObject select;
 
+    private void Start()
+    {
+        
+    }
+
+    // 스테이지 선택 시 해당 스테이지에서 시작
     public void startSelectedStage(int stageIndex)
     {
         DataManager.Instance.level = stageIndex;
